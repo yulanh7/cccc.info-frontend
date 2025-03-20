a68006a83367cd28d00c67351c4634b18e248084
Yulan Huang <yulanh7@gmail.com>
2025-03-20 13:53:18 +1100

    initial commit with Next.js, ts, jest, and tailwindcss

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: 'babel',
+  testEnvironment: 'jsdom',
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
