06bb1aca5fbeae9820c0aa36f60cef8459c4c617
Yulan Huang <yulanh7@gmail.com>
2025-03-22 01:23:36 +1100

    adjust the style of single post page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mockPostList } from '@/app/data/mockData'
+import { CalendarIcon, UserCircleIcon, UserGroupIcon } from '@heroicons/react/24/outline';
 
 interface Post {
   id: number;
@@ -22,20 +23,26 @@ export default function PostDetailPage({ params }: { params: { id: string } }) {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-6">{post.title}</h1>
       <div className="aspect-w-16 aspect-h-9 mb-4">
         <iframe
           src={post.videoUrl}
           title={post.title}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-          className="w-full h-full rounded-lg"
+          className="w-full h-[200px] md:h-[400px] rounded-lg"
         ></iframe>
       </div>
-      <div className="text-sm text-gray-600 mb-4">
-        <span>{post.date}</span> • <span>{post.author}</span> • <span>{post.group}</span>
+      <h1 className="text-2xl mb-2">{post.title}</h1>
+      <div className="text-xs text-dark-green md:text-sm mb-1 flex items-center">
+        <UserGroupIcon className="h-4 w-4 mr-1 text-dark-green" />{post.group}
       </div>
-      <p className="text-gray-700">{post.description}</p>
+      <div className="text-xs text-dark-green md:text-sm mb-1 flex items-center">
+        <UserCircleIcon className="h-4 w-4 mr-1 text-dark-green" /> {post.author}
+      </div>
+      <div className="text-xs text-dark-green md:text-sm mb-4 flex items-center">
+        <CalendarIcon className="h-4 w-4 mr-1 text-dark-green" /> {post.date}
+      </div>
+      <p className="text-gray">{post.description}</p>
     </div>
   );
 }
