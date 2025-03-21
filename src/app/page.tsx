1eb26eadda5495223843fe95e6617a383483a722
Yulan Huang <yulanh7@gmail.com>
2025-03-22 00:56:45 +1100

    change the style of post box in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 import Post from '@/components/Post';
 import { mockPostList } from '@/app/data/mockData'
 
-
 export default function HomePage() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-6 mt-6 flex justify-center align-middle">Home</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="container mx-auto md:p-6 p-1">
+      <div className="columns-2 gap-1 md:columns-2 lg:columns-3 md:gap-8">
         {mockPostList.map((post) => (
-          <Post
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            date={post.date}
-            author={post.author}
-            group={post.group}
-            description={post.description}
-            videoUrl={post.videoUrl}
-          />
+          <div className="break-inside-avoid mb-1 md:mb-8">
+            <Post
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              date={post.date}
+              author={post.author}
+              group={post.group}
+              description={post.description}
+              videoUrl={post.videoUrl}
+            />
+          </div>
         ))}
       </div>
     </div>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { PostProps } from '@/app/types/post';
-
+import { CalendarIcon, UserCircleIcon, UserGroupIcon } from '@heroicons/react/24/outline';
 
 
 
@@ -8,37 +8,36 @@ export default function Post({ id, title, date, author, group, description, vide
   const truncatedDescription = description.length > 100 ? `${description.slice(0, 100)}...` : description;
 
   return (
-    <Link href={`/posts/${id}`} className="block">
-      <div className="bg-white p-6 rounded-lg shadow-md md:rounded-lg md:shadow-md md:p-6">
-        {/* YouTube 视频 */}
-        <div className="aspect-w-16 aspect-h-9 mb-4">
-          <iframe
-            src={videoUrl}
-            title={title}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            className="w-full h-full rounded-lg md:rounded-lg"
-          ></iframe>
+    <div className="bg-white shadow-md md:shadow-md relative rounded-xs md:rounded-sm">
+      <div className="aspect-w-16 aspect-h-9 mb-1.5 md:mb-4">
+        <iframe
+          src={videoUrl}
+          title={title}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="w-full h-[160px] md:h-[200px] rounded-t-xs md:rounded-t-sm"
+        ></iframe>
+      </div>
+      <div className='p-1.5 md:p-3'>
+        <h2 className="text-sm md:text-xl mb-1 md:mb-2">{title}</h2>
+        <div className="text-xs text-dark-gray md:text-sm mb-1 flex items-center">
+          <UserGroupIcon className="h-4 w-4 mr-1 text-dark-gray" />{group}
         </div>
-
-        {/* 标题 */}
-        <h2 className="text-xl font-bold mb-2">{title}</h2>
-
-        {/* 元信息 */}
-        <div className="text-sm text-gray-600 mb-4">
-          <span>{date}</span> • <span>{author}</span> • <span>{group}</span>
+        <div className="text-xs text-dark-gray md:text-sm mb-1 flex items-center">
+          <UserCircleIcon className="h-4 w-4 mr-1 text-dark-gray" /> {author}
         </div>
-
-        {/* 描述 */}
-        <p className="text-gray-700 mb-4 md:mb-4">{truncatedDescription}</p>
-
-        {/* 按钮（仅中大屏幕显示） */}
-        <div className="hidden md:block">
-          <button className="w-full bg-yellow text-black py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors">
-            View Details
-          </button>
+        <div className="text-xs text-gray md:text-sm text-gray-600 md:mb-2 flex items-center">
+        </div>
+        <p className="hidden md:block text-gray md:mb-4 md:mb-4">{truncatedDescription}</p>
+        <Link href={`/posts/${id}`} className="block">
+          <h3 className="text-xs md:text-base text-dark-green underline hover:text-dark-gray transition-colors">
+            Read More
+          </h3>
+        </Link>
+        <div className="absolute left-2.5 top-1/3 transform -translate-x-1/2 -translate-y-1/2 -rotate-90 bg-yellow text-dark-gray text-xs md:text-sm flex items-center justify-center px-2 py-0">
+          <CalendarIcon className="h-4 w-4 mr-1" /> {date}
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
