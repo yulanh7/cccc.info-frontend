a770872f8cbc45a43954e884a9014642fa3f5cc7
Yulan Huang <yulanh7@gmail.com>
2025-03-24 17:57:35 +1100

    feat: add PageTitle to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 import Post from '@/components/Post';
 import { mockPostList } from '@/app/data/mockData'
+import PageTitle from '@/components/PageTitle';
 
 export default function HomePage() {
   return (
-    <div className="container mx-auto md:p-6 p-1">
-      <div className="columns-2 gap-1 md:columns-2 lg:columns-3 md:gap-8">
-        {mockPostList.map((post) => (
-          <div className="break-inside-avoid mb-1 md:mb-8" key={post.id}>
-            <Post
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              date={post.date}
-              author={post.author}
-              group={post.group}
-              description={post.description}
-              videoUrl={post.videoUrl}
-            />
-          </div>
-        ))}
+    <>
+      <PageTitle title="Home" showPageTitle={true} />
+      <div className="container mx-auto md:p-6 p-4 md:mt-20">
+        <div className="columns-2 gap-1 md:columns-2 lg:columns-3 md:gap-8">
+          {mockPostList.map((post) => (
+            <div className="break-inside-avoid mb-1 md:mb-8" key={post.id}>
+              <Post
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                date={post.date}
+                author={post.author}
+                group={post.group}
+                description={post.description}
+                videoUrl={post.videoUrl}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
