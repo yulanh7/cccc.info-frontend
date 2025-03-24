5df8d1fd584227dc6d1be926650474dc06613d6f
Yulan Huang <yulanh7@gmail.com>
2025-03-24 17:02:32 +1100

    fix: add key to post map in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <div className="container mx-auto md:p-6 p-1">
       <div className="columns-2 gap-1 md:columns-2 lg:columns-3 md:gap-8">
         {mockPostList.map((post) => (
-          <div className="break-inside-avoid mb-1 md:mb-8">
+          <div className="break-inside-avoid mb-1 md:mb-8" key={post.id}>
             <Post
               key={post.id}
               id={post.id}
