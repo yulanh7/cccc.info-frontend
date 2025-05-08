fd28d948eb276966c38d966058fb4537caf55655
Yulan Huang <yulanh7@gmail.com>
2025-05-08 17:30:13 +1000

    feat: Add Redux Provider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css';
 import Header from '@/components/Header';
 import BottomNav from '@/components/BottomNav';
-
+import { Provider } from 'react-redux';
+import { store } from '@/app/features/store';
 
 export default function RootLayout({
   children,
@@ -17,11 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-bg text-dark-gray min-h-screen flex flex-col">
-        <Header isLoggedIn={isLoggedIn} userName={userName} unreadCount={unreadCount} />
-        <main className="bg-bg pb-16">
-          {children}
-        </main>
-        <BottomNav unreadCount={unreadCount} />
+        <Provider store={store}>
+          <Header isLoggedIn={isLoggedIn} userName={userName} unreadCount={unreadCount} />
+          <main className="bg-bg pb-16">
+            {children}
+          </main>
+          <BottomNav unreadCount={unreadCount} />
+        </Provider>
       </body>
     </html>
   );
