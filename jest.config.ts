17a92ff6c66061bcc21a187d5ac980c5caddc4cf
Yulan Huang <yulanh7@gmail.com>
2025-05-09 15:10:45 +1000

    feat: add jest setup file and update jest config and package file for jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,20 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+/** @type {import('jest').Config} */
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
-// Add any custom config to be passed to Jest
-const config: Config = {
-  coverageProvider: 'babel',
-  testEnvironment: 'jsdom',
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-}
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+module.exports = createJestConfig(customJestConfig);
