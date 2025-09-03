c0887cbbfd3ac3e00c8aa29ef360ac26230a2ffe
Yulan Huang <yulanh7@gmail.com>
2025-09-03 14:26:34 +1000

    feat: change api url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const origin = process.env.NEXT_PUBLIC_BACKEND_ORIGIN || 'http://localhost:5000';
     return [
       { source: '/api/:path*', destination: 'http://localhost:5000/api/:path*' },
       { source: '/files/:path*', destination: 'http://localhost:5000/files/:path*' }, // ✅
