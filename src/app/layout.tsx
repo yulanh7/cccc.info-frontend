93838539026be64621a6566d1907030dbac4e445
Yulan Huang <yulanh7@gmail.com>
2025-09-11 23:48:04 +1000

    chore: update favicon to custom PNG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Canberra Chinese Christian Church',
   description: '',
+  icons: {
+    icon: '/images/logo.png',
+  },
 };
 
 export default function RootLayout({
